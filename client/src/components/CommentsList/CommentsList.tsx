e663b1e5ed6e514d5bd859e0c283d0e755136518
ChongRen <chongren.lee@ufinity.com>
2021-08-03 17:53:43 +0800

    [CR] add bottom sheet on add comment button

--- client/src/components/CommentsList/CommentsList.tsx
+++ client/src/components/CommentsList/CommentsList.tsx
@@ -1,17 +1,18 @@
 import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { FC, useState } from "react";
+import Sheet from "react-modal-sheet";
 
 import { SortBy } from "../../constants";
 import { Comment } from "../../entities/Comment";
 import CommentsListItem from "../CommentsListItem";
-
 interface Props {
   comments: Comment[];
 }
 
 const CommentsList: FC<Props> = ({ comments }) => {
   const [sortBy, setSortBy] = useState<SortBy>(SortBy.TOP);
+  const [isOpen, setOpen] = useState(false);
 
   const numComments = comments.length;
 
@@ -21,6 +22,10 @@ const CommentsList: FC<Props> = ({ comments }) => {
     } else {
       setSortBy(SortBy.TOP);
     }
+  };
+
+  const handleAddComment = () => {
+    setOpen(true);
   };
 
   return (
@@ -34,7 +39,66 @@ const CommentsList: FC<Props> = ({ comments }) => {
             />
           </button>
         </div>
-        <button className="p-1 my-2 text-base font-bold bg-yellow-300 rounded-full">
+        <>
+          <Sheet
+            isOpen={isOpen}
+            onClose={() => setOpen(false)}
+            snapPoints={[400]}
+            initialSnap={0}
+          >
+            <Sheet.Container>
+              <Sheet.Content
+                style={{
+                  justifyContent: "center",
+                  display: "block",
+                }}
+              >
+                <textarea
+                  style={{
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    borderRadius: "10px",
+                    border: "2px solid #F9D040",
+                    outline: "none",
+                    padding: "10px",
+                    height: "80px",
+                    marginTop: "16px",
+                    display: "flex",
+                    width: "90%",
+                    resize: "none",
+                  }}
+                  placeholder="Add a comment..."
+                  onFocus={(e) => (e.target.placeholder = "")}
+                  onBlur={(e) => (e.target.placeholder = "Add a comment...")}
+                />
+                <button
+                  onClick={handleAddComment}
+                  className="p-1 my-2 text-base font-bold bg-yellow-300 rounded-full"
+                  style={{
+                    justifyContent: "center",
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    borderRadius: "25px",
+                    border: "2px solid #F9D040",
+                    outline: "none",
+                    padding: "3px",
+                    marginTop: "16px",
+                    display: "flex",
+                    width: "90%",
+                    resize: "none",
+                  }}
+                >
+                  Post
+                </button>
+              </Sheet.Content>
+            </Sheet.Container>
+            <Sheet.Backdrop />
+          </Sheet>
+        </>
+        <button
+          onClick={handleAddComment}
+          className="p-1 my-2 text-base font-bold bg-yellow-300 rounded-full"
+        >
           Add a comment
         </button>
       </div>
