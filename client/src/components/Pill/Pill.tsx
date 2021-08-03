11a0754bd3bde16c178435f50b0f739c4b325dc5
Tan Yi Long <yilong.tan94@gmail.com>
2021-08-03 21:23:48 +0800

    Add modal for new poll

--- client/src/components/Pill/Pill.tsx
+++ client/src/components/Pill/Pill.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const Pill: FC<Props> = ({ text }) => {
   return (
-    <div className="inline-block px-2 py-1 text-xs bg-indigo-400 rounded-full">
+    <div className="inline-block px-2 py-1 text-xs font-bold bg-yellow-300 rounded">
       {text}
     </div>
   );
