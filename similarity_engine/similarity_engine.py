986709757d87e8c7e1ed19a753f04dbfe9f44128
Seah Ming Shu <mingshuseah@gmail.com>
2021-08-04 20:19:24 +0800

    add threshold

--- similarity_engine/similarity_engine.py
+++ similarity_engine/similarity_engine.py
@@ -4,14 +4,16 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 
 class SimilarityEngine:
-    def __init__(self, min_df=1, stop_words='english', number_of_results=3):
+    def __init__(self, min_df=1, stop_words='english', number_of_results=3, similarity_threshold=0.2):
         self.vectorizer = TfidfVectorizer(min_df=min_df, stop_words=stop_words)
         self.number_of_results = number_of_results
+        self.similarity_threshold = similarity_threshold
 
     def run(self, input_doc, corpus):
         corpus.append(input_doc)
         tfidf = self.vectorizer.fit_transform(corpus)
 
-        cos_sim = cosine_similarity(tfidf[-1], tfidf[:-1])
+        cos_sim = cosine_similarity(tfidf[-1], tfidf[:-1])[0]
 
-        return np.argsort(-cos_sim)[0][:self.number_of_results].tolist()
+        result = np.argsort(-cos_sim)[:self.number_of_results]
+        return [x for x in result if cos_sim[x] > self.similarity_threshold]
