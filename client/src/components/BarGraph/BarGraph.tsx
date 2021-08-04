2e70f0521f7653b8e0a2931553a3f0e542290c84
Tan Yi Long <yilong.tan94@gmail.com>
2021-08-04 11:41:49 +0800

    Format bar graph

--- client/src/components/BarGraph/BarGraph.tsx
+++ client/src/components/BarGraph/BarGraph.tsx
@@ -1,21 +1,31 @@
+import classNames from "classnames";
 import { CSSProperties, FC } from "react";
 
 interface BarColumnProps {
   value: number;
   label: string;
+  color?: string;
+  selected?: boolean;
 }
 
-const BarColumn: FC<BarColumnProps> = ({ value, label }) => {
+const BarColumn: FC<BarColumnProps> = ({
+  value,
+  label,
+  color = "bg-gray-300",
+  selected,
+}) => {
   const roundedValue = Math.round(value * 100);
 
   const style: CSSProperties = {
     width: `${roundedValue}%`,
   };
 
+  const barClass = classNames("flex items-center h-5", color);
+
   return (
     <div className="flex items-center">
       <div className="flex-grow h-5 bg-white">
-        <div style={style} className="flex items-center h-5 bg-gray-400">
+        <div style={style} className={barClass}>
           <span className="ml-2 text-xs">{label}</span>
         </div>
       </div>
@@ -31,12 +41,23 @@ interface Props {
 }
 
 const BarGraph: FC<Props> = ({ data, question }) => {
+  const highestValue = Math.max(...Object.values(data));
+
   return (
-    <div className="p-2 border border-black">
+    <div className="p-2 border-2 border-black rounded-md">
       <div className="mb-2 text-sm font-bold">{question}</div>
       <div className="space-y-1">
         {Object.keys(data).map((label, index) => {
-          return <BarColumn key={index} label={label} value={data[label]} />;
+          const color =
+            highestValue === data[label] ? "bg-yellow-300" : "bg-gray-300";
+          return (
+            <BarColumn
+              key={index}
+              label={label}
+              value={data[label]}
+              color={color}
+            />
+          );
         })}
       </div>
     </div>
