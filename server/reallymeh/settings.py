3b61b341fcbf9c3f8013eccd6bc67daf4cef968a
Tan Yi Long <yilong.tan94@gmail.com>
2021-08-05 12:12:49 +0800

    Deploy to heroku

--- server/reallymeh/settings.py
+++ server/reallymeh/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3$w1)6oj&7f35han-4(nim=(jnl&^&%&ir71@jpw@bs^%oa-cm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 # Application definition
 
